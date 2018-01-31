23cfd125fad7587278e44fb61d001b601cca0a22
Juciara Lima <juciara.slima@gmail.com>
2018-01-31 17:59:37 +0000

    tabela ife corrigida

--- js/function.js
+++ js/function.js
@@ -4,7 +4,7 @@
 
 function Funcionalidades(){
   
-var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Login</a>'},
+var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Login</a>',"categoria":"back end","ator":'aluno'},
         {func:"F2", "descricao":'<a href="#" class="btnF2">Cadastro de Empresa</a>'},
         {func:"F3", "descricao":'<a href="#" class="btnF3">Cadastro de Vaga</a>'},
         {func:"F4", "descricao":'<a href="#" class="btnF4">Filtro de Candidados</a>'},
@@ -22,14 +22,20 @@ var tabela = document.getElementById("tabela");
 for (var i in obj){
     
     var tr = document.createElement("tr");
-    var tdProduto = document.createElement("td");
-    var tdPreco = document.createElement("td");
+    var tdFuncionalidade = document.createElement("td");
+    var tdDescricao = document.createElement("td");
+    var tdCategoria = document.createElement("td");
+    var tdAtor = document.createElement("td");
     var tbody = document.querySelector("tbody");
     
-    tdProduto.innerHTML = obj[i].func;
-    tdPreco.innerHTML = obj[i].descricao;
-    tr.appendChild(tdProduto)
-    tr.appendChild(tdPreco);
+    tdFuncionalidade.innerHTML = obj[i].func;
+    tdDescricao.innerHTML = obj[i].descricao;
+    tdCategoria.innerHTML = obj[i].categoria;
+    tdAtor.innerHTML = obj[i].ator;
+    tr.appendChild(tdFuncionalidade)
+    tr.appendChild(tdDescricao);
+    tr.appendChild(tdCategoria);
+    tr.appendChild(tdAtor);
     tbody.appendChild(tr);
     tabela.appendChild(tbody);
 }
