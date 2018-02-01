a596f860aa427b3ee59d75da5a03c5eebc23d237
Andressa Rocha <andressarochamorgado@hotmail.com>
2018-02-01 18:15:13 +0000

    Funcionalidades IFE

--- js/function.js
+++ js/function.js
@@ -4,18 +4,18 @@
 
 function Funcionalidades(){
   
-var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Login</a>',"categoria":"back end","ator":'aluno'},
-        {func:"F2", "descricao":'<a href="#" class="btnF2">Cadastro de Empresa</a>'},
-        {func:"F3", "descricao":'<a href="#" class="btnF3">Cadastro de Vaga</a>'},
-        {func:"F4", "descricao":'<a href="#" class="btnF4">Filtro de Candidados</a>'},
-        {func:"F5", "descricao":'<a href="#" class="btnF5">Lista de Alunos</a>'},
-        {func:"F6", "descricao":'<a href="#" class="btnF6">Lista de Empresas</a>'},
-        {func:"F7", "descricao":'<a href="#" class="btnF7">Excluir Vaga</a>'},
-        {func:"F8", "descricao":'<a href="#" class="btnF8">Alter Empresa</a>'},
-        {func:"F9", "descricao":'<a href="#" class="btnF9">Alterar Vaga</a>'},
-        {func:"F10", "descricao":'<a href="#" class="btnF10">Excluir vaga</a>'},
-        {func:"F11", "descricao":'<a href="#" class="btnF11">Desabilitar Empresa</a>'},
-        {func:"F12", "descricao":'<a href="#" class="btnF12">FeedBack</a>'}];
+var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Login</a>',"categoria":"Back e Front","ator":'Adm/Empresa'},
+        {func:"F2", "descricao":'<a href="#" class="btnF2">Cadastro de Empresa</a>',"categoria":"Back e Front","ator":'Adm'},
+        {func:"F3", "descricao":'<a href="#" class="btnF3">Cadastro de Vaga</a>',"categoria":"Back e Front","ator":'Empresa'},
+        {func:"F4", "descricao":'<a href="#" class="btnF4">Filtro de Candidados</a>',"categoria":"Back e Front","ator":'Empresa'},
+        {func:"F5", "descricao":'<a href="#" class="btnF5">Lista de Alunos</a>',"categoria":"Back e Front","ator":'Adm'},
+        {func:"F6", "descricao":'<a href="#" class="btnF6">Lista de Empresas</a>',"categoria":"Back e Front","ator":'Adm'},
+        {func:"F7", "descricao":'<a href="#" class="btnF7">Excluir Vaga</a>',"categoria":"Back e Front","ator":'Empresa'},
+        {func:"F8", "descricao":'<a href="#" class="btnF8">Alterar Empresa</a>',"categoria":"Back e Front","ator":'Empresa'},
+        {func:"F9", "descricao":'<a href="#" class="btnF9">Alterar Vaga</a>',"categoria":"Back e Front","ator":'Empresa'},
+        {func:"F10", "descricao":'<a href="#" class="btnF10">Desabilitar vaga</a>',"categoria":"Back e Front","ator":'Empresa'},
+        {func:"F11", "descricao":'<a href="#" class="btnF11">Desabilitar Empresa</a>',"categoria":"Back e Front","ator":'Adm'},
+        {func:"F12", "descricao":'<a href="#" class="btnF12">FeedBack</a>',"categoria":"Back e Front","ator":'Empresa'}];
  
 var tabela = document.getElementById("tabela");
 
