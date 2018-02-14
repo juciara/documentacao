440c2a6ec654e86c27375032406390c2981347d6
Andressa Rocha <andressarochamorgado@hotmail.com>
2018-02-14 18:54:12 +0000

    Criando funções js das func do SITE

--- js/function.js
+++ js/function.js
@@ -68,6 +68,159 @@ for (var i in obj){
     tr.appendChild(tdAtor);
     tbody.appendChild(tr);
     tabela.appendChild(tbody);
-}
+    }
 }
 
+function formatura(){
+  
+var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Cadastrar </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F2", "descricao":'<a href="#" class="btnF12">Excluir </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F3", "descricao":'<a href="#" class="btnF12">Alterar </a>',"categoria":"Back","ator":'Adm'}];
+ 
+var tabela = document.getElementById("tabela");
+
+for (var i in obj){
+    
+    var tr = document.createElement("tr");
+    var tdFuncionalidade = document.createElement("td");
+    var tdDescricao = document.createElement("td");
+    var tdCategoria = document.createElement("td");
+    var tdAtor = document.createElement("td");
+    var tbody = document.querySelector("tbody");
+    
+    tdFuncionalidade.innerHTML = obj[i].func;
+    tdDescricao.innerHTML = obj[i].descricao;
+    tdCategoria.innerHTML = obj[i].categoria;
+    tdAtor.innerHTML = obj[i].ator;
+    tr.appendChild(tdFuncionalidade)
+    tr.appendChild(tdDescricao);
+    tr.appendChild(tdCategoria);
+    tr.appendChild(tdAtor);
+    tbody.appendChild(tr);
+    tabela.appendChild(tbody);
+    }
+}
+
+function maratona(){
+  
+var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Cadastrar </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F2", "descricao":'<a href="#" class="btnF12">Excluir </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F3", "descricao":'<a href="#" class="btnF12">Alterar </a>',"categoria":"Back","ator":'Adm'}];
+ 
+var tabela = document.getElementById("tabela");
+
+for (var i in obj){
+    
+    var tr = document.createElement("tr");
+    var tdFuncionalidade = document.createElement("td");
+    var tdDescricao = document.createElement("td");
+    var tdCategoria = document.createElement("td");
+    var tdAtor = document.createElement("td");
+    var tbody = document.querySelector("tbody");
+    
+    tdFuncionalidade.innerHTML = obj[i].func;
+    tdDescricao.innerHTML = obj[i].descricao;
+    tdCategoria.innerHTML = obj[i].categoria;
+    tdAtor.innerHTML = obj[i].ator;
+    tr.appendChild(tdFuncionalidade)
+    tr.appendChild(tdDescricao);
+    tr.appendChild(tdCategoria);
+    tr.appendChild(tdAtor);
+    tbody.appendChild(tr);
+    tabela.appendChild(tbody);
+    }
+}
+
+function semana(){
+  
+var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Cadastrar </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F2", "descricao":'<a href="#" class="btnF12">Excluir </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F3", "descricao":'<a href="#" class="btnF12">Alterar </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F4", "descricao":'<a href="#" class="btnF12">Inscrição</a>',"categoria":"Back e front","ator":'Aluno'},
+            {func:"F5", "descricao":'<a href="#" class="btnF12">Gerar certificado </a>',"categoria":"Back e front","ator":'Adm'},
+            {func:"F6", "descricao":'<a href="#" class="btnF12">Obter certificado </a>',"categoria":"Back e front","ator":'Aluno'}];
+ 
+var tabela = document.getElementById("tabela");
+
+for (var i in obj){
+    
+    var tr = document.createElement("tr");
+    var tdFuncionalidade = document.createElement("td");
+    var tdDescricao = document.createElement("td");
+    var tdCategoria = document.createElement("td");
+    var tdAtor = document.createElement("td");
+    var tbody = document.querySelector("tbody");
+    
+    tdFuncionalidade.innerHTML = obj[i].func;
+    tdDescricao.innerHTML = obj[i].descricao;
+    tdCategoria.innerHTML = obj[i].categoria;
+    tdAtor.innerHTML = obj[i].ator;
+    tr.appendChild(tdFuncionalidade)
+    tr.appendChild(tdDescricao);
+    tr.appendChild(tdCategoria);
+    tr.appendChild(tdAtor);
+    tbody.appendChild(tr);
+    tabela.appendChild(tbody);
+    }
+}
+
+
+function projetos(){
+  
+var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Cadastrar </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F2", "descricao":'<a href="#" class="btnF12">Excluir </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F3", "descricao":'<a href="#" class="btnF12">Alterar </a>',"categoria":"Back","ator":'Adm'}];
+ 
+var tabela = document.getElementById("tabela");
+
+for (var i in obj){
+    
+    var tr = document.createElement("tr");
+    var tdFuncionalidade = document.createElement("td");
+    var tdDescricao = document.createElement("td");
+    var tdCategoria = document.createElement("td");
+    var tdAtor = document.createElement("td");
+    var tbody = document.querySelector("tbody");
+    
+    tdFuncionalidade.innerHTML = obj[i].func;
+    tdDescricao.innerHTML = obj[i].descricao;
+    tdCategoria.innerHTML = obj[i].categoria;
+    tdAtor.innerHTML = obj[i].ator;
+    tr.appendChild(tdFuncionalidade)
+    tr.appendChild(tdDescricao);
+    tr.appendChild(tdCategoria);
+    tr.appendChild(tdAtor);
+    tbody.appendChild(tr);
+    tabela.appendChild(tbody);
+    }
+}
+
+function setores(){
+  
+var obj = [{func:"F1", "descricao":'<a href="#" class="btnF1">Cadastrar </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F2", "descricao":'<a href="#" class="btnF12">Excluir </a>',"categoria":"Back","ator":'Adm'},
+            {func:"F3", "descricao":'<a href="#" class="btnF12">Alterar </a>',"categoria":"Back","ator":'Adm'}];
+ 
+var tabela = document.getElementById("tabela");
+
+for (var i in obj){
+    
+    var tr = document.createElement("tr");
+    var tdFuncionalidade = document.createElement("td");
+    var tdDescricao = document.createElement("td");
+    var tdCategoria = document.createElement("td");
+    var tdAtor = document.createElement("td");
+    var tbody = document.querySelector("tbody");
+    
+    tdFuncionalidade.innerHTML = obj[i].func;
+    tdDescricao.innerHTML = obj[i].descricao;
+    tdCategoria.innerHTML = obj[i].categoria;
+    tdAtor.innerHTML = obj[i].ator;
+    tr.appendChild(tdFuncionalidade)
+    tr.appendChild(tdDescricao);
+    tr.appendChild(tdCategoria);
+    tr.appendChild(tdAtor);
+    tbody.appendChild(tr);
+    tabela.appendChild(tbody);
+    }
+}
